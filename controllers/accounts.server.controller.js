2b542418d055ab933a01d315221169563d9db4b8
Mohammed Gomaa <mohammed.gomaa9484@gmail.com>
2018-08-05 13:25:03 +0200

    users changed

--- controllers/accounts.server.controller.js
+++ controllers/accounts.server.controller.js
@@ -1,6 +1,5 @@
 var Accounts = require('../models/accounts.server.model.js');
 exports.get = function (req, res) {
-   
     var limit = parseInt(req.query.$limit) || 10;
     var skip = parseInt(req.query.$skip) || 0;
     let total=0;
@@ -10,17 +9,10 @@ exports.get = function (req, res) {
             searchQuery = { "number": new RegExp(req.query.number.$like) }
     }
     var query = Accounts.find(searchQuery).sort({ number: 'desc' }).limit(limit).skip(skip);
-    
     Accounts.find(searchQuery).count().exec().then(function (count) {
         total=count;
         query.exec(function (err, results) {
-          
-            
             res.json({ skip, limit,total, data: results });
         });
         });
-
-   
-   
-
 } 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
+class Vehicle {
+  constructor() {
+  }
+  static getCar() {
+    console.log("vehicle");
+  }
+
+}
+class Drone extends Vehicle {
+
+  constructor() {
+   super();
+
+  }
+  static getCar() {
+    super.getCar();
+    console.log("dron");
+  }
+}
+
+drone = new Drone();
+Drone.getCar();
+
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.send({"msisdn":95022881,"subscriptionDate":"2018-05-29 15:01:45.908","subscribed":true});
 });
 
 module.exports = router;
